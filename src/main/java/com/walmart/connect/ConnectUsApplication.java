9da4840e9af6b21ee78644cb93c049c9796853d5
shivam srivastava <s0s0boq@HOMEOFFICE.wal-mart.com>
2021-01-16 19:18:17 +0530

    Initial commit

---
+++ src/main/java/com/walmart/connect/ConnectUsApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/connect/ConnectUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
