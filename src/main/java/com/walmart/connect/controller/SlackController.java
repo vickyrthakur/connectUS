c7175d900ae113af37cbd51d26ffacfee2ab6f03
Vicky Thakur - v0t00fa <Vicky.Thakur@walmartlabs.com>
2021-01-17 13:40:22 +0530

    make it better

--- src/main/java/com/walmart/connect/controller/SlackController.java
+++ src/main/java/com/walmart/connect/controller/SlackController.java
@@ -37,11 +37,37 @@ public class SlackController {
                                                @RequestParam("text") String text,
                                                @RequestParam("response_url") String responseUrl) {
         SlackResponse response = new SlackResponse();
-        response.setText("This is the response text");
+        response.setText("status for :"+text+"is:");
         response.setResponseType("in_channel");
 
         Attachment attachment = new Attachment();
         attachment.setText("This is the attachment text");
+        attachment.setColor("#0000ff");
+
+        response.attachments.add(attachment);
+
+        return response;
+    }
+
+
+    @RequestMapping(value = "/slack/connect",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public SlackResponse onReceiveSlashCommandforConncet(@RequestParam("team_id") String teamId,
+                                               @RequestParam("team_domain") String teamDomain,
+                                               @RequestParam("channel_id") String channelId,
+                                               @RequestParam("channel_name") String channelName,
+                                               @RequestParam("user_id") String userId,
+                                               @RequestParam("user_name") String userName,
+                                               @RequestParam("command") String command,
+                                               @RequestParam("text") String text,
+                                               @RequestParam("response_url") String responseUrl) {
+        SlackResponse response = new SlackResponse();
+        response.setText(text);
+        response.setResponseType("in_channel");
+
+        Attachment attachment = new Attachment();
+        attachment.setText("This is the pannel  info");
         attachment.setColor("#0000ff");
 
         response.attachments.add(attachment);
